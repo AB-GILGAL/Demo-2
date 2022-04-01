b27793634b9c842e2e0eba300fac0d8cb000c1d3
AB-GILGAL <ocansey89ao.ao@gmail.comm>
2022-04-01 22:39:42 +0000

    first commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>AB'S Grid</h1>
+      <h2>Beautiful Memorable Pictures</h2>
+      <p>It's very OK to look back.</p>
+    </div>
+  )
+}
+
+export default Title;
